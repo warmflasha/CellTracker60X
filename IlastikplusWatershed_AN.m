a70ff3998b19af6296be0a8c22cec318fb3b5f3e
Anastasiia <nmnemashkalo@gmail.com>
2015-10-27 18:52:52 -0500

    need to streamline the analysis

--- IlastikplusWatershed_AN.m
+++ IlastikplusWatershed_AN.m
@@ -1,4 +1,4 @@
-function [statsnuc,statscyto, cellsinframe,Lnuc,Lcyto] = IlastikplusWatershed_AN(ilastikfile,pos,zplane,direc,flag)
+function [statsnuc,statscyto,Lnuc,Lcyto] = IlastikplusWatershed_AN(ilastikfile,pos,zplane,direc)
 
 ff=readAndorDirectory(direc);
 chan = ff.w;
@@ -9,7 +9,7 @@ userParam.gaussSigma = 3;
 userParam.small_rad = 3;
 userParam.presubNucBackground = 1;
 userParam.backdiskrad = 300;
-areanuclow = 700;
+areanuclow = 1200;
 areanuchi = 9000;
 
 info = h5info(ilastikfile);
@@ -21,9 +21,9 @@ data = squeeze(data);
 time = size(data,3);                                   % how many time frames were expoted ( depends on the dataset, set in ilastik)
 Lnuc = {};
 Lcyto = {};
-for k=3:time
+for k=1:time
     
-Lnuc{k} = data(:,:,k) >1;  % need to leave only the nuclei masks and make the image binary
+Lnuc{k} = data(:,:,k) <2;  % need to leave only the nuclei masks and make the image binary
 Lnuc{k} =  bwareafilt(Lnuc{k}',[areanuclow areanuchi]);
 
 filename = getAndorFileName(ff,pos,ff.t(k),ff.z(zplane),chan(2)); % has to be channel 2 since all the masks should be applied to the gfp channel
@@ -53,26 +53,27 @@ statscyto{k} = regionprops(cc_cyto{k},I2,'Area','Centroid','PixelIdxList','MeanI
 acyto{k} = [statscyto{k}.Area];
 xcyto = aa(1:2:end);
 ycyto = aa(2:2:end);
+end
 
-nucmeanInt{k}  = [statsnuc{k}.MeanIntensity]; %k is the index over images!!!!! statsnuc is a cell array
-cytomeanInt{k}  = [statscyto{k}.MeanIntensity];
+% nucmeanInt{k}  = [statsnuc{k}.MeanIntensity]; %k is the index over images!!!!! statsnuc is a cell array
+% cytomeanInt{k}  = [statscyto{k}.MeanIntensity];
 
 
 % To show which objects are labeled in the final watershed segmentation in nuc
 % lear and cyto channels; also show the final foreground markers
-if flag == 1
-    Lrgb = label2rgb(Lnuc{k}, 'jet', 'k', 'shuffle');
-    figure,subplot(1,2,1),imshow(I2,[]);hold on
-    h = imshow(Lrgb);
-    h.AlphaData = 0.3;
-    Lrgbcyto = label2rgb(Lcyto{k}, 'jet', 'k', 'shuffle');
-    subplot(1,2,2),imshow(I2,[]);hold on
-    h = imshow(Lrgbcyto);
-    h.AlphaData = 0.3;
-end
-
-%plot the results
-
+% if flag == 1
+%     Lrgb = label2rgb(Lnuc{k}, 'jet', 'k', 'shuffle');
+%     figure,subplot(1,2,1),imshow(I2,[]);hold on
+%     h = imshow(Lrgb);
+%     h.AlphaData = 0.3;
+%     Lrgbcyto = label2rgb(Lcyto{k}, 'jet', 'k', 'shuffle');
+%     subplot(1,2,2),imshow(I2,[]);hold on
+%     h = imshow(Lrgbcyto);
+%     h.AlphaData = 0.3;
+% end
+% 
+% %plot the results
+% 
 % ratios{k}(:,1) = nucmeanInt{k}./cytomeanInt{k};
 % ratios{k}(:,2) = k;
 % ratios{k}(:,3) = xnuc';
@@ -88,16 +89,16 @@ end
 % ylabel('nuc/cyto mean for cells in the frame');
 % title('GFPsmad4RFPh2b cells, 10ng/ml bmp4 added after frame 16, ~ 9 hours imaging time');
 % legend('Mean over all cells in frame') ;
-% 
-% 
-% %  twocellcol1{k} = mean(ratios{k}(2:3,1));
-% %  twocellcol2{k} = mean(ratios{k}(2:3,1));
-%  
-%   onecellcol1{k} = ratios{k}(1,1);
-%   onecellcol2{k} = ratios{k}(2,1); % 
-%   %onecellcol3{k} = ratios{k}(3,1); % 
-% 
-% %figure(5),plot(k,twocellcol{k},'b--*');hold on
+
+
+%  twocellcol1{k} = mean(ratios{k}(2:3,1));
+%  twocellcol2{k} = mean(ratios{k}(2:3,1));
+ 
+ % onecellcol1{k} = ratios{k}(1,1);
+ % onecellcol2{k} = ratios{k}(2,1); % 
+  %onecellcol3{k} = ratios{k}(3,1); % 
+
+%figure(5),plot(k,twocellcol{k},'b--*');hold on
 % figure(5),plot(k,meanRa{k},'r--*','Markersize',15);hold on
 % %plot(k,onecellcol3{k},'b--*');
 % plot(k,onecellcol1{k}, 'm--*');
@@ -110,12 +111,12 @@ end
 
 
 
-end
+
 
 %  figure(4), savefig('MeanDynamicsframe10.fig');
 %  figure(5),savefig('CellTracesframe10.fig');
 % 
-  save('Frame13_Analysis');
+  save('Frame004_Analysis');
 
 end
 
