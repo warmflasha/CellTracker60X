55ef629cbd07d5d3265d37f0c88642ceeda7c682
Anastasiia <nmnemashkalo@gmail.com>
2015-11-09 18:48:31 -0600

    some useful generalizations

--- TestScript60X.m
+++ TestScript60X.m
@@ -7,19 +7,19 @@ cd(direccurr);
 direc = ('/Users/warmflashlab/Desktop/A_NEMASHKALO_Data_and_stuff/9_LiveCllImaging/SingleCellSignalingAN_20150805_123245 PM');
 %direc = ('/Volumes/data/Anastasia/LiveCellImagingGFPs4RFPh2b/SingleCellSignalingAN_20150805_123245 PM');
 flag = 0;
-ilastikfile = 'NucMask0001batch.h5';
-ilastikfilecyto = 'CytoMask0001batch.h5';
-pos = 18;
+ilastikfile = 'NucMask0004batch.h5';
+ilastikfilecyto = 'CytoMask0004batch.h5';
+pos = 4;
 zplane = 4;
-img = 7;
+img = 1;
 
 %to run img = 1,first image
 
 
- %[datacell,Lnuc,Lcytofin] = IlastikplusWatershed_AW(ilastikfile,ilastikfilecyto,pos,zplane,direc,img,flag);
+ [datacell,Lnuc,Lcytofin] = IlastikplusWatershed_AW(ilastikfile,ilastikfilecyto,pos,zplane,direc,img,flag);
 
 
 % to run all time points
 % 
-[peaks,dims,NucMasks,CytoMasks] = RunTimeSeries60XuColoniesAN(ilastikfile,ilastikfilecyto,pos,zplane,direc,flag);
+%[peaks,dims,NucMasks,CytoMasks] = RunTimeSeries60XuColoniesAN(ilastikfile,ilastikfilecyto,pos,zplane,direc,flag);
 
