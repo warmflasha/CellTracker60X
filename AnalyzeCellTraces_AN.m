5c7a05a891c5a6ae9710e2d8de7b5c289bbe18fc
anemashkalo <nmnemashkalo@gmail.com>
2015-12-07 00:11:31 -0600

    added plot sign jump amplitude into analysis

--- AnalyzeCellTraces_AN.m
+++ AnalyzeCellTraces_AN.m
@@ -1,5 +1,5 @@
 
-function [datcell,mean_before,mean_after] = AnalyzeCellTraces_AN(dir,col,N,fr_stim,delta_t)
+function [datcell,mean_before,mean_after,found_cells,amplitude] = AnalyzeCellTraces_AN(dir,col,N,fr_stim,delta_t)
 
 % plot cell traces for specific colonies using the output from the runTracker
 % EDS and runcolonygrouping
@@ -16,9 +16,9 @@ function [datcell,mean_before,mean_after] = AnalyzeCellTraces_AN(dir,col,N,fr_st
 % the cell belongs to
 % 
 
-[nums, files]=folderFilesFromKeyword(dir,'Outfile_0016');%['Outfile_000' num2str(pos) '_tps']
+[nums, files]=folderFilesFromKeyword(dir,'Outfile_');%['Outfile_000' num2str(pos) '_tps']
 
-
+%found_cells = cell(length(peaks),1);
 for j=1:length(nums)
 
 matfile = files(j).name;
@@ -26,12 +26,12 @@ matfile = files(j).name;
 pp = load(matfile,'peaks','cells'); % AN
 peaks = pp.peaks;
 vect{j} = 1:length(peaks);
-%found_cells{j} = cell(1,length(peaks));
+
 vect{j} = (vect{j}.*delta_t)./60;% x axis in units of hours  
 
 for k=1:length(peaks)
 if ~isempty(peaks{k})
-%found_cells(:,1) = size(peaks{k},1);% how many cells within each frame were found    
+ 
 trN(k) = max(peaks{k}(:,8));
 end
 end
@@ -39,7 +39,8 @@ trN = max(trN);
 trN =(1:trN);
 
  %trN = max(peaks{1}(:,4)); % number of trajectories within the frame
-c = {'r','g','b','k','m','c','m','k'};
+%c = {'r','g','b','k','m','c','m','k'};
+colors = jet(length(nums));
 alldata=zeros(length(peaks),length(trN));%length(trN)
  
 for xx = 1:length(trN)
@@ -52,7 +53,7 @@ for xx = 1:length(trN)
                 
                 if peaks{k}(i,9) == N && peaks{k}(i,8) == trN(xx);%
                     alldata(k,xx) = peaks{k}(i,col(1))./peaks{k}(i,col(2));
-                    
+                    found_cells{j}(k,1) = size(peaks{k},1);% how many cells within each frame were found  
                 end
                 
             end
@@ -69,17 +70,20 @@ p = fr_stim*delta_t/60;
 for j=1:length(datcell)
     
     for k=1:size(datcell{j},2)
-        if  length(nonzeros(datcell{j}(:,k)))>30
-            figure(1),plot(vect{j},datcell{j}(:,k),'*','color',c{j});
+        if  length(nonzeros(datcell{j}(:,k)))>30%40
+            figure(1),plot(vect{j},datcell{j}(:,k),'*','color',colors(j,:));
+            avgsign(j) = mean(datcell{j}(:,k));
             legend(['bmp4 added at ' num2str(p) 'hours']);
             hold on
-                        
+                      
         end
         
     end
+    
     mean_before{j} = mean(nonzeros(datcell{j}(1:fr_stim,:)));
     mean_after{j} =  mean(nonzeros(datcell{j}((fr_stim+1):end,:)));  
-    %amplitude{j} =  datcell{j}((fr_stim+1):end,:))
+    amplitude{j}(:,:) = abs(mean(nonzeros(datcell{j}((fr_stim-1),:)))- mean(nonzeros(datcell{j}((fr_stim+3),:))));%5 for 5min delta_t
+   % amplitude{j} = mean_before{j}-mean_after{j};
 end
 
     ylim([0 2.4])
@@ -89,25 +93,33 @@ end
     
     mean_before = cell2mat(mean_before);
     mean_after = cell2mat(mean_after);
-    
-    figure(2),subplot(1,2,1),plot(mean_before,'b*','markersize',20);
+    amplitude = cell2mat(amplitude);
+    figure(2),subplot(1,3,1),plot(mean_before,'*','color',colors(j,:),'markersize',20);
     hold on
     ylim([0 1.8]);
     xlim([0 length(datcell)]);
      legend('before');
-     ylabel('nuc/cyto raio, mean over time');
+     ylabel('nuc/cyto ratio, mean over time');
     xlabel('Positions');
-    title(['microCol of size ' num2str(N) ],'fontsize',15);
-     figure(2),subplot(1,2,2),plot(mean_after,'m*','markersize',20);
+   % title(['microCol of size ' num2str(N) ' ,all subplots'],'fontsize',15);
+     figure(2),subplot(1,3,2),plot(mean_after,'*','color',colors((j-1),:),'markersize',20);
     hold on
     legend('after');
      ylim([0 1.8])
     xlim([0 length(datcell)]);
-    ylabel('nuc/cyto raio, mean over time');
+    ylabel('nuc/cyto ratio, mean over time');
     xlabel('Positions');
-    title(['microCol of size ' num2str(N) ],'fontsize',15);
+    title(['microCol of size ' num2str(N) ' ,all subplots'],'fontsize',15);
+    figure(2),subplot(1,3,3),plot(amplitude,'*','color',colors((j-2),:),'markersize',20);
+    hold on
+    legend('amplitude');
+     ylim([0 0.5])
+    xlim([0 length(datcell)]);
+    ylabel('nuc/cyto ratio change, mean over cells in frame','fontsize',9);
+    xlabel('Positions');
+  %  title(['microCol of size ' num2str(N) ],'fontsize',15);
     
-%save(['CellTraces_' num2str(N) ],'datcell','mean_before','mean_after');
+%save(['CellTraces_' num2str(N) ],'datcell','mean_before','mean_after','found_cells','amplitude');
 
 
 end
