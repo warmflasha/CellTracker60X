372ab5979239b9ba728de89c2b6196d1d1cba578
Anastasiia <nmnemashkalo@gmail.com>
2016-02-22 18:25:30 -0600

    figure1panels in new combined code

--- DirInfo_TrackMicroCol.m
+++ DirInfo_TrackMicroCol.m
@@ -3,7 +3,7 @@
 
 ilastikdircyto = ('/Users/warmflashlab/Desktop/A_NEMASHKALO_Data_and_stuff/9_LiveCllImaging/3Dsegmentation_tracking_TrainingSet/masks_zcyto');
 ilastikdir = ('/Users/warmflashlab/Desktop/A_NEMASHKALO_Data_and_stuff/9_LiveCllImaging/3Dsegmentation_tracking_TrainingSet/Masks_z2');
-imagedir = ('/Users/warmflashlab/Desktop/A_NEMASHKALO_Data_and_stuff/9_LiveCllImaging/3Dsegmentation_tracking_TrainingSet/rawimages_');
+imagedir = ('/Users/warmflashlab/Desktop/A_NEMASHKALO_Data_and_stuff/9_LiveCllImaging/3Dsegmentation_tracking_TrainingSet/');%rawimages_
 pos = 23;
 tpt =57;
 timegroup = 1;
