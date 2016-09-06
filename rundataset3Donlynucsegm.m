69e60451ef2d88cf61e4654719c6f5dd5a130ba6
Anastasiia <nmnemashkalo@gmail.com>
2016-09-06 13:34:36 -0500

    functions updated to do ilasik segmentation withour cyto masks

---
+++ rundataset3Donlynucsegm.m
@@ -0,0 +1,51 @@
+function rundataset3Donlynucsegm(ilastikdirnuc,imagedir,pos,paramfile,timegroup,outfile,paramfile3D,pl,strnuc,chanal)
+
+% run all timepoints and save into one peaks
+% main fnction
+% pl = number of different ilastik files corresponding to the same position
+% (either z planes or time groups, etc.)
+% strnuc = keyword within the folder of nuc masks to separate the masks
+% corresponding only to single position 'pos' ( e.g. give position may have masks
+% separately for each x z plane)
+%
+
+    % get the specific position ilastik masks (all z projections)
+[ilastikNucAll] = FindPositionMasks(ilastikdirnuc,pl,(pos-1),strnuc);
+imgfiles = struct;
+
+
+nTprev = 0;
+   
+% read raw images
+[imgsnuc_reader]   =  getrawimgfiles(imagedir,pl, (pos-1),timegroup,chanal(1));   % get the raw images for that position and merge them into a 3d format
+ 
+nT = imgsnuc_reader{1}.getSizeT;                                                    % how many time point are within given time group
+
+for k =1  % loop over time points the fixed images have only one datapoint
+        
+    % read pnuc and pcyto separately from images
+    [pnuc]=readmaskfiles1(ilastikNucAll,k,2); % readmaskfiles1(ilastikNucAll,k,lblN)    lblN = which ilastik label to use as cell nuc. and which as cyto
+        
+    for m = 1:size(imgsnuc_reader,2) %
+        planenuc = imgsnuc_reader{m}.getIndex(0,0, k - 1) + 1;
+        inuc(:,:,m) = bfGetPlane(imgsnuc_reader{m},planenuc);   % make sure take the correct plane m (not just the first one)
+               
+    end
+         
+        [outdat,Lnuc] = runmaskoneANdataonlynucsegm(pnuc,inuc,paramfile,paramfile3D,imagedir,pl,pos,timegroup);
+        
+        peaks{nTprev+k} = outdat;
+        
+        imgfiles(nTprev+k).NucMask = (Lnuc(:,:,round(size(Lnuc,3)/2))); %(Lnuc(:,:,1:size(Lnuc,3)))%round(size(Lnuc,3)/2) compressBinaryImg(Lnuc(:,:,3)) round(size(Lnuc,3)/2) compressBinaryImg
+        disp([k (pos-1)]);
+    
+end
+
+% nTprev = nTprev + nT;% this is only if there are several time groups
+% to put into the same peaks
+
+
+save([ num2str(pos-1) '_' num2str(outfile)],'peaks','imgfiles');
+
+
+
---
+++ untitled4.m
@@ -0,0 +1,111 @@
+function [outdat,Lnuc,Lcytofin] = runmaskoneANdataonlynucsegm(pnuc,pcyto, inuc,icyto,timegroup,paramfile,paramfile3D)
+
+
+eval(paramfile);
+eval(paramfile3D);
+global userParam;
+
+% sapna code tracking 
+pmasks = primaryfilterAN(pnuc,userParam.probthresh_nuc, userParam.area1filter);
+
+% here can insert the UnmergeTwoNuclei function ( after the masks are
+% already binary
+
+% zrange: where the nuclei are in z
+[zrange] = secondaryfilterAN(pmasks, userParam.minstartobj);
+smasks = pmasks;
+%[zrange, smasks] = secondaryfilterAN(pmasks, userParam.minstartobj, userParam.minsolidity);
+ if zrange == 0;
+    outdat = [];
+    Lnuc = pmasks(:,:,1);
+    Lcytofin = im2bw(pcyto(:,:,1),userParam.probthresh_cyto);
+    disp('zrange is zero1');
+    return
+ end
+% here can find the max object in the image and then based on this info set
+% the parameter for the minimum area to unmerge 
+
+
+if userParam.flag ==1
+    for k=1:size(smasks,3) % test once for 40X
+        [~,smasks(:,:,k)] = UnmergetwonucleiGeneral(smasks(:,:,k));
+        [~,smasks(:,:,k)] = UnmergetwonucleiGeneral(smasks(:,:,k));
+    end
+    
+    % do again, to make sure that if three cells were merged, they are cut
+    % after the second round of Unmerge2nuclei
+    
+%     for k=1:size(smasks,3)           
+%         [~,smasks(:,:,k)] = UnmergetwonucleiGeneral(smasks(:,:,k));
+%     end
+end
+
+if userParam.flag ==0
+    disp('no unmerge')
+end
+% get the acual tracking nucleilist = tracked objects, labled 1-N,CC -
+% pixelidxlist of all objects tracked in all planes
+[PILsn, PILsSourcen, CC, masterCCn, stats, nuclein1, zrange] = traceobjectszdistinct(smasks, userParam.matchdistance, zrange, size(zrange,2));%size(zrange,2)userParam.zmatch
+
+if ~iscell(CC) 
+  pmasks = primaryfilterAN(pnuc,userParam.probthresh_nuc, userParam.area1filter);
+  [zrange, smasks] = secondaryfilterAN(pmasks, userParam.minstartobj);
+  if zrange == 0;
+    outdat = [];
+    Lnuc = pmasks(:,:,1);
+    Lcytofin = im2bw(pcyto(:,:,1),0.9);
+    disp('zrange is zero2');
+    return
+end
+  [PILsn, PILsSourcen, CC, masterCCn, stats, nuclein1, zrange] = traceobjectszdistinct(smasks, userParam.matchdistance, zrange, size(zrange,2));%size(zrange,2)userParam.zmatch  
+     
+end
+if zrange == 0;
+    outdat = [];
+    Lnuc = smasks(:,:,1);
+    Lcytofin = im2bw(pcyto(:,:,1),0.9);
+    disp('zrange is zero3');
+    return
+end
+ % use nucleilist to relabel the tracked objects with unique labels
+[newmask_lbl] = lblmask_3Dnuc(CC,nuclein1);
+
+% [nucleilist, masterCC] =  overlapfilter(PILsn, PILsSourcen, masterCCn, nucleilist, inuc, zrange, userParam.overlapthresh);
+
+% leave only planes with cells in the cyto channel too
+goodk = zeros(size(zrange,2),1);
+ for k=1:size(zrange,2)
+ N = size(nuclein1,1);
+ a =  find(isnan(nuclein1(:,k)));
+ 
+ if size(a,1)< N 
+     goodk(k,1) = k;
+ end
+ goodk = nonzeros(goodk);
+ end
+  
+ for k=1:size(goodk,1)
+ %pmaskscyto1(:,:,k) = im2bw(pcyto(:,:,zrange(goodk(k))),userParam.probthresh_cyto);
+ pmaskscyto(:,:,k) = imfill(pcyto(:,:,zrange(goodk(k)))> userParam.probthresh_cyto,'holes');
+ %pmaskscyto(:,:,k) = bwareafilt(pmaskscyto(:,:,k),[userParam.areacytolow userParam.areacytolow*100]);           % here filter the cyto masks by area ( remove realy huge cytoplasms )
+ icyto_new(:,:,k) =icyto(:,:,zrange(goodk(k)));                         % inuc, icyto = still have 5 layers instack, need to leave only the ones
+ inuc_new(:,:,k) =inuc(:,:,zrange(goodk(k)));                           % with the nuclei in them ( as was determined by zrange)
+ end
+ 
+ 
+
+ % get the labeled cyto masks ( nuclei not subtracted in the
+ % GetVoronoiCells3D function since need them filled in order to remove
+ % cytos without nuc)
+ 
+ [maskzcyto_lbl] = GetVoronoiCells3D(newmask_lbl,pmaskscyto);
+ 
+ % now maskzcyto and newmask are 3D masks that need to be applied to
+ % icyto,inuc, which are all now of the same dim
+ % all the masks are labeled
+   
+ [datacell,Lnuc,Lcytofin] = nucCytoIlastik2peaks_3Dsegm(newmask_lbl,maskzcyto_lbl,inuc_new,icyto_new,paramfile,zrange);
+ outdat = datacell;
+ 
+ 
+end
